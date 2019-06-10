30a5a148e7505db9e6b03397dbdf9a754f5fef7c
Konservator-mn <fryb08179@gmail.com>
2019-06-10 20:28:31 +0300

    Created User model

---
+++ server/database/Schemas/User.js
@@ -0,0 +1,31 @@
+const   mongoose = require('../connect'),
+        crypto = require('crypto');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 6
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    salt: {
+        type: String,
+        required: true
+    }
+});
+userSchema.methods.setPassword = function (password){
+    this.salt = crypto.randomBytes(128).toString('hex');
+    this.password = crypto.pbkdf2Sync(password, this.salt, 1000, 64, `sha512`).toString(`hex`);
+};
+userSchema.methods.validPassword = function (password){
+    let incomePassword = crypto.pbkdf2Sync(password, this.salt, 1000, 64, `sha512`).toString(`hex`);
+    return this.password === incomePassword;
+};
+
+
+module.exports = mongoose.model('User', userSchema);
--- server/database/index.js
+++ server/database/index.js
@@ -1,15 +1,3 @@
-const   mongoose = require('mongoose'),
-        uri = "mongodb+srv://danitinstagram:danitinstagram@dan-it-instagram-qznrg.mongodb.net/test?retryWrites=true&w=majority";
-
-mongoose.connect(uri, {useNewUrlParser: true});
-
-const connection = mongoose.connection;
-
-connection.on('error', console.error);
-
-connection.once('open', function() {
-    console.log('Connected to database');
-});
-
-module.exports = {mongoose, connection};
-
+module.exports = function (modelName) {
+    return require(`./Wrap/${modelName}`);
+};
